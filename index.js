2821720869231cbe4fac655b3a6552332b0c8053
Lizzie <lchan217@gmail.com>
2019-05-31 11:19:14 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function theTruth(){
+  return true
+}
+
+function sayHey(){
+  return "Hey!"
+}
+
+function favoriteIceCream(flavor) {
+  return `I love ${flavor}`;
+}
+
+function sayHeyFriend(friend){
+  return `Hey, ${friend}!` 
+}
